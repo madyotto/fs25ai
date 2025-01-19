4e176929e0ce29cea987d59e97ebba8b67af0c5c
madyotto <50303849+madyotto@users.noreply.github.com>
2025-01-19 21:43:59 +0000

    Add files via upload

---
+++ run_dataset_creator.bat
@@ -0,0 +1,3 @@
+@echo off
+python create_dataset.py --schema_dir "D:\Games\Farming Simulator 25\shared\xml\schema" --doc_dir "D:\Games\Farming Simulator 25\shared\xml\documentation" --output_dir "output"
+pause
